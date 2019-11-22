eb185af8df23d11628db3a0e0d45e54d9bc66d84
webturing <zj@webturing.com>
2019-11-22 08:43:38 +0800

    week12 数组应用素数筛法

---
+++ lec14array/环形数组.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a[] = {1, 2, 3, 4, 5};
+  int k = 2999;
+  for (int i = 0; i < 5; i++) {
+    cout << a[(i + k) % 5] << " ";
+  }
+  return 0;
+}
---
+++ lec14array/素数测试.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 200000;
+bool prime(int n) {  // version 0: O(n)
+  for (int i = 2; i <= n - 1; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+bool goldbach(int n) {
+  for (int p = 3; p <= n / 2; p += 2) {
+    if (prime(p) && prime(n - p)) {
+      // cout<<p<<"+"<<n-p<<"="<<n<<endl;
+      return true;
+    }
+  }
+  return false;
+}
+
+int main() {
+  ll start = clock();
+  for (int n = 6; n <= N; n += 2) {
+    if (!goldbach(n)) {
+      cout << "Congratulations! The goldbach theory is wrong!" << endl;
+    }
+  }
+
+  ll end = clock();
+  cout << (end - start) / 1000.0 << " Second(s)." << endl;
+  return 0;
+}
